0625e4cc6ab4f3329d182a5be6a9217523f6d309
rachi <rachiashtekar345@gmail.com>
2024-03-15 21:27:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-// import React, { useState } from 'react';
-// import './App.css';
-// import Home from './Home/Home';
-// import CreateNotesAndPopup from './createNotesandPopup/CreateNotesandPopup';
-
-// function App() {
-//   const [selectedGroup, setSelectedGroup] = useState(null);
-
-//   const handleGroupSelect = (group) => {
-//     setSelectedGroup(group);
-//   };
-
-//   return (
-//     <div>
-//       <CreateNotesAndPopup onGroupSelect={handleGroupSelect} />
-//       <Home selectedGroup={selectedGroup} />
-//     </div>
-//   );
-// }
-
-// export default App;
-import React, { useState } from 'react';
-import './App.css';
-import Home from './Home/Home';
-import CreateNotesAndPopup from './createNotesandPopup/CreateNotesandPopup';
+import React, { useState } from "react";
+import "./App.css";
+import Home from "./Home/Home";
+import CreateNotesAndPopup from "./createNotesandPopup/CreateNotesandPopup";
 
 function App() {
   const [selectedGroup, setSelectedGroup] = useState(null);
--- src/NotesContainer/NotesContainer.js
+++
@@ -1,8 +0,0 @@
-function NotesContainer(){
-    return (
-        <div>
-
-        </div>
-    )
-}
-export default NotesContainer
